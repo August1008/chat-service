b0dbd80183233338b5307c9a106b34f924d6edcc
August1008 <ngbinhan1512@gmail.com>
2024-12-29 10:57:06 +0700

    Add project files.

---
+++ chat-application-1/ChatHub/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace chat_application_1.ChatHub
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageAsync(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ chat-application-1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace chat_application_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
